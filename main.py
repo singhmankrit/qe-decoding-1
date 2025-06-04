5571409f253f3f7d578e28ad9fa426fc728e2d92
Mankrit Singh <singhmankrit@gmail.com>
2025-06-04 13:28:18 +0200

    start problem 2

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from simulator import (
+from problem_1 import (
     simulate_threshold_mv,
     simulate_threshold_mwpm,
 )
 
+# Problem 1
+
 # Majority Voting
-threshold_mv, _, _ = simulate_threshold_mv(n_runs=10**4)
-print(f"Estimated threshold (Majority Voting): {threshold_mv}")
+# threshold_mv, _, _ = simulate_threshold_mv(n_runs=10**4)
+# print(f"Estimated threshold (Majority Voting): {threshold_mv}")
 
 # Minimum Weight Perfect Matching
-threshold_mwpm, _, _ = simulate_threshold_mwpm(n_runs=10**4)
-print(f"Estimated threshold (MWPM): {threshold_mwpm}")
+# threshold_mwpm, _, _ = simulate_threshold_mwpm(n_runs=10**4)
+# print(f"Estimated threshold (MWPM): {threshold_mwpm}")
+
+# Problem 2
