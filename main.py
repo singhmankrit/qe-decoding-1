ca4842fea25156acc16e272c83ae212b1c78ad37
Mankrit Singh <54510715+singhmankrit@users.noreply.github.com>
2025-06-04 13:38:42 +0200

    fix: Refactor Code for Problem 2 (#4)
    * start problem 2
    
    * problem 1 images
    
    * refactor
    
    * docstrings

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from simulator import (
-    simulate_threshold_mv,
-    simulate_threshold_mwpm,
-)
+from simulation_code import problem_1
 
-# Majority Voting
-threshold_mv, _, _ = simulate_threshold_mv(n_runs=10**4)
-print(f"Estimated threshold (Majority Voting): {threshold_mv}")
+# Problem 1
 
-# Minimum Weight Perfect Matching
-threshold_mwpm, _, _ = simulate_threshold_mwpm(n_runs=10**4)
-print(f"Estimated threshold (MWPM): {threshold_mwpm}")
+# # Majority Voting
+# threshold_mv, _, _ = problem_1.simulate_threshold_mv(n_runs=10**4)
+# print(f"Estimated threshold (Majority Voting): {threshold_mv}")
+
+# # Minimum Weight Perfect Matching
+# threshold_mwpm, _, _ = problem_1.simulate_threshold_mwpm(n_runs=10**4)
+# print(f"Estimated threshold (MWPM): {threshold_mwpm}")
+
+# Problem 2
