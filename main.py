4495d1d595ade8b86452949d8e229b933311f23b
Mankrit Singh <singhmankrit@gmail.com>
2025-06-11 17:08:53 +0200

    comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from simulation_code import problem_1, problem_2
 
+# ==================================================================
 # Problem 1
+# ==================================================================
 
 # Majority Voting
 threshold_mv, _, _ = problem_1.simulate_threshold_mv(n_runs=10**6)
@@ -10,7 +12,9 @@ print(f"Estimated threshold (Majority Voting): {threshold_mv}")
 threshold_mwpm = problem_1.simulate_threshold_mwpm(n_runs=10**6)
 print(f"Estimated threshold (MWPM): {threshold_mwpm}")
 
+# ==================================================================
 # Problem 2
+# ==================================================================
 
 # Minimum Weight Perfect Matching for Multiple Ancilla Measurements
 threshold_w_ancilla = problem_2.simulate_threshold(n_runs=10**6)
